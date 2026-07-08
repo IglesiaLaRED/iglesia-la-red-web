7698df2d4bed115d8e0f7031619d70f4cc609cdc
IglesiaLaRED <iglesialared3@gmail.com>
2026-07-08 14:48:13 -0600

    Update configClinica.js

--- js/configClinica.js
+++ js/configClinica.js
@@ -1,1 +1,17 @@
-
+const CONFIG_CLINICA = {
+  version: "1.0",
+  areas: [
+    // aquí va toda la lista oficial de áreas y preguntas
+  ],
+  alertasCriticas: [
+    "violacion",
+    "abuso sexual",
+    "aborto",
+    "suicidio",
+    "brujeria",
+    "hechiceria",
+    "ocultismo",
+    "oraciones satanicas",
+    "pactos"
+  ]
+};
