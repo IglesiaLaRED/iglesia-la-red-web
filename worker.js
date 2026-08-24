0e793d5dc0f412e6c5fba9dac005b7bea77924f5
IglesiaLaRED <iglesialared3@gmail.com>
2026-08-24 14:53:35 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,27 @@
+export default {
 
+  async fetch(request, env) {
+
+    const url = new URL(request.url);
+
+    // ==========================================
+    // YOUTARS API
+    // ==========================================
+    if (url.pathname === "/api/youtars") {
+
+      return Response.json({
+        ok: true,
+        sistema: "YouTARS",
+        estado: "ONLINE"
+      });
+
+    }
+
+    // ==========================================
+    // SITIO WEB NORMAL
+    // ==========================================
+    return env.ASSETS.fetch(request);
+
+  }
+
+};
